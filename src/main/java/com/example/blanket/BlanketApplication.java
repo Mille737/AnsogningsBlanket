831b98e4f181d1d526480fc7a8fc761e8a757b56
Camille Ellen Grace Sandidge <mille@Camilles-MacBook-Pro.local>
2019-09-25 20:13:55 +0200

    initial commit

---
+++ src/main/java/com/example/blanket/BlanketApplication.java
@@ -0,0 +1,13 @@
+package com.example.blanket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlanketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlanketApplication.class, args);
+    }
+
+}
